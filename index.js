b93c50410c3ef58edb65bc84b448cd7d42ee5e1f
BonziUSER <156539692+BonziUSER@users.noreply.github.com>
2026-05-10 11:01:30 -0300

    whole site was broken so i reverted back to where it worked

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+// ========================================================================
+// Server init
+// ========================================================================
+
+// Filesystem reading functions
 const fs = require('fs-extra');
 
+// Load settings
 try {
 	stats = fs.lstatSync('settings.json');
 } catch (e) {
+	// If settings do not yet exist
 	if (e.code == "ENOENT") {
 		try {
 			fs.copySync(
@@ -14,15 +21,21 @@ try {
 			console.log(e);
 			throw "Could not create new settings file.";
 		}
+	// Else, there was a misc error (permissions?)
 	} else {
 		console.log(e);
 		throw "Could not read 'settings.json'.";
 	}
 }
 
+// Load settings into memory
 const settings = require("./settings.json");
+// Setup basic express server
 var express = require('express');
 var app = express();
+if (settings.express.serveStatic)
+	app.use(express.static('./build/www'));
+var server = require('http').createServer(app);
 var https = require('https');
 const VPNAPI_KEY = 'cd10f82529fc4b5b87a8c4f51ca0d186';
 
@@ -76,23 +89,25 @@ const checkVPN = async (req, res, next) => {
     }
 };
 
-app.use(checkVPN);
-if (settings.express.serveStatic)
-    app.use(express.static('./build/www'));
-var server = require('http').createServer(app);
+// Init socket.io
 var io = require('socket.io')(server);
 var port = process.env.PORT || settings.port;
 
 exports.io = io;
 
+// Init sanitize-html
 var sanitize = require('sanitize-html');
 
+// Init winston loggers (hi there)
 const Log = require('./log.js');
 Log.init();
 const log = Log.log;
 
+// Load ban list
 const Ban = require('./ban.js');
 Ban.init();
+
+// Start actually listening
 server.listen(port, function () {
 	console.log(
 		"\n",
@@ -102,99 +117,24 @@ server.listen(port, function () {
 	);
 });
 app.use(express.static(__dirname + '/public'));
-app.use(express.json());
 
-app.get('/api/v1/', async (req, res) => res.sendStatus('hello world'))
-app.get('/api/v1/rooms/',  async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./rooms.json')));
-})
-app.post('/api/v1/rooms/',  async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./rooms.json')));
-})
-app.get('/api/v1/identity/user/', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./user.json')));
-})
-app.post('/api/v1/identity/user/', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./user.json')));
-})
-app.get('/api/v1/identity/fingerprint/', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./fingerprint.json')));
-})
-app.post('/api/v1/identity/fingerprint/', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./fingerprint.json')));
-})
-app.get('/api/v1/session/', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./session.json')));
-})
-app.post('/api/v1/session/', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./session.json')));
-})
-app.get('/api/v1/login/', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./logins.json')));
-})
-app.post('/api/v1/login/', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./logins.json')));
-})
-app.get('/api/v1/login/register/', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./register.json')));
-}) 
-app.post('/api/v1/login/register/', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./register.json')));
-}) 
-app.get('/api/v1/login/forgot/', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./forgot.json')));
-})
-app.post('/api/v1/login/forgot/', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./forgot.json')));
-})
-app.get('/api/v1/unload/', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./unload.json')));
-})
-app.post('/api/v1/unload/', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(require('./unload.json')));
-})
+// ========================================================================
+// Banning functions
+// ========================================================================
 
-
-app.post( "/api/v2/login/", async ( req, res ) => {
-    try {
-        const user = await User.findByCredentials(
-            req.body.email,
-            req.body.username,
-            req.body.password
-        );
-        const token = await user.generateAuthToken();
-        res.send( {
-            user,
-            token,
-        } );
-    } catch ( e ) {
-        res.status( 400 ).send( {
-            error: "Catch error",
-            e,
-        } );
-    }
-} );
+// ========================================================================
+// Helper functions
+// ========================================================================
 
 const Utils = require("./utils.js")
+
+// ========================================================================
+// The Beef(TM)
+// ========================================================================
 
 const Meat = require("./meat.js");
 Meat.beat();
 
+// Console commands
 const Console = require('./console.js');
 Console.listen();
