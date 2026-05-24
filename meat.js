c14197d2fafcd7b6196f6d72f586b4d0134d9a7d
izhank216 <izhank216@gmail.com>
2026-05-24 19:40:51 +0500

    Update meat.js

--- meat.js
+++ meat.js
@@ -299,6 +299,9 @@ let userCommands = {
     "joke": function() {
         if (this.room) this.room.emit("joke", { guid: this.guid, rng: Math.random() });
     },
+    "dvdbounce": function() {
+        if (this.room) socket.emit("dvdbounce", { guid: this.guid, rng: Math.random() });
+    },
     "fact": function() {
         if (this.room) this.room.emit("fact", { guid: this.guid, rng: Math.random() });
     },
